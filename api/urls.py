c0c2135dae84905bdffa37c01d22b46b2c0c6de5
Didier Dardevet <didier.dardevet@gmail.com>
2021-09-27 09:58:17 +0200

    add issue and comments views/urls

--- api/urls.py
+++ api/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('projects/<int:pk>/users/', views.ContributorList.as_view(), name='project-contributors'),
     path('projects/<int:pk>/users/<int:pk2>/', views.ContributorDetail.as_view(), name='project-contributors-detail'),
     path('projects/<int:pk>/issues/', views.IssueList.as_view(), name='project-issues'),
-    # path('projects/<int:pk>/issues/<int:pk2>/', views.issue_detail, name='project_issue-detail'),
-    # path('projects/<int:pk>/issues/<int:pk2>/comments/', views.comment_list, name='issue-comments'),
-    # path('projects/<int:pk>/issues/<int:pk2>/comments/<int:pk3>/', views.comment_detail, name='issue-comment-detail'),
-
+    path('projects/<int:pk>/issues/<int:pk2>/', views.IssueDetail.as_view(), name='project_issue-detail'),
+    path('projects/<int:pk>/issues/<int:pk2>/comments/', views.CommentList.as_view(), name='issue-comments'),
+    path('projects/<int:pk>/issues/<int:pk2>/comments/<int:pk3>/', views.CommentDetail.as_view(),
+         name='issue-comment-detail'),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -198,13 +198,13 @@ class IssueList(APIView):
 
     def get_object(self, pk):
         try:
-            return Issue.objects.get(pk=pk)
-        except Issue.DoesNotExist:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         project = self.get_object(pk=pk)
-        self.check_object_permissions(request, obj=project)
+        # self.check_object_permissions(request, obj=project)
         issues = Issue.objects.filter(project_id=project.project_id)
         serializer = IssueSerializer(issues, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -221,12 +221,72 @@ class IssueDetail(APIView):
     """
     Retrieve, update or delete an Issue instance.
     """
+    def get_object(self, pk):
+        try:
+            return Issue.objects.get(pk=pk)
+        except Issue.DoesNotExist:
+            raise Http404
 
-    def get(self, request, pk, *args, **kwargs):
-        pass
+    def get(self, request, pk2, *args, **kwargs):
+        issue = self.get_object(pk=pk2)
+        serializer = IssueSerializer(issue)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk, *args, **kwargs):
-        pass
+    def put(self, request, pk2, *args, **kwargs):
+        issue = self.get_object(pk2)
+        serializer = IssueSerializer(issue, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, *args, **kwargs):
-        pass
+    def delete(self, request, pk2, *args, **kwargs):
+        issue = self.get_object(pk2)
+        issue.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentList(APIView):
+    """List of all comments related to an issue, or create a new comment."""
+    def get(self, request, pk2, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=pk2)
+        comments = Comment.objects.filter(issue_id=issue)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+    """
+    Retrieve, update or delete a comment.
+    """
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk3, *args, **kwargs):
+        comment = self.get_object(pk=pk3)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk3, *args, **kwargs):
+        comment = self.get_object(pk=pk3)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk3, *args, **kwargs):
+        comment = self.get_object(pk=pk3)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
